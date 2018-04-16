bf234f8ee11e37af3223e5d1556b3c757b221d6b
Mary Ellen Weber <mmweber2@gmail.com>
2018-04-16 18:54:49 +0900

    Add solving and displaying of solved board.
    Accept a single row of input and fill the rest with zeroes.
    Print solved board to console.
    Print solved board to page.

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, url_for, request
 from flask import render_template
 import board
+from solver import fill_board
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    if not request.args:
+        print "Didn't find a request."
     row1 = request.args.get('row1', '')
     row1 += "0" * 72
     try:
-        print board.Board.string_to_array(row1)
+        board_array = board.Board.string_to_array(row1)
     except ValueError:
-        print "Board {} is not 81 characters.".format(row1)
-    return render_template('index.html')
+        error = "Board {} is not 81 characters.".format(row1)
+        print error
+        return render_template('index.html', error=error)
+    b = board.Board(board_array)
+    solved = str(fill_board(b))
+    print 'Solved board is '
+    print solved
+    return render_template('index.html', output=solved)
