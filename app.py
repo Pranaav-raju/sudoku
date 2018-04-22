376fdeac58caad3ca2a687fe712a7e044ebb64fe
Mary Ellen Weber <mmweber2@gmail.com>
2018-04-22 15:43:01 +0900

    Add initial support for puzzle argument
    Add check to app.py to recognize a 'puzzle' argument.
    If the string is < 81 characters, pad the rest with zeroes.

--- app.py
+++ app.py
@@ -7,22 +7,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # If request.args has items, but not 81 of them, it's likely that someone appended
+    # If request.args has non-puzzle items, but not 81 of them, it's likely that someone appended
     #   invalid values to the URL.
     # It's possible that someone just specified some of the cells and left the rest out
     #   of the argument list, but I don't see value in catering to that possibility.
-    if not request.args or len(request.args) != 81:
+    if not request.args:
         print "Didn't find a request."
         return render_template('grid.html')
-    board_letters = []
-    for row in xrange(9):
-        for col in xrange(9):
-            cell = request.args.get(str(row) + str(col), '')
-            if cell == "" or cell == "0":
-                board_letters.append("0")
+    if 'puzzle' in request.args:
+        puzzle_string = request.args.get('puzzle')
+        if len(puzzle_string) < 81:
+            # Pad short string with trailing zeroes
+            puzzle_string += "0" * (81 - len(puzzle_string))
+        board_array = Board.string_to_array(puzzle_string)
+    elif len(request.args) >= 81:
+        board_letters = []
+        for row in xrange(9):
+            for col in xrange(9):
+                cell = request.args.get(str(row) + str(col), '')
+                if cell == "" or cell == "0":
+                    board_letters.append("0")
             else:
                 board_letters.append(cell)
-    board_array = Board.string_to_array("".join(board_letters))
+        board_array = Board.string_to_array("".join(board_letters))
     b = Board(board_array)
     try:
         solved = solve(b)
