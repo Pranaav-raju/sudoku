61a85a6661e1286905c141f9cf4f98d78515fb7d
Mary Ellen Weber <mmweber2@gmail.com>
2018-04-22 19:04:32 +0900

    Refactor argument parsing in index()
    Move checking of all individual cells into its own method
        build_puzzle_string().
    Add ‘error’ variable to keep track of the error message.
    Present error messages for puzzle_string longer than 81 characters.
    Present error message for puzzle_string with invalid characters.
    Pad < 81 character puzzle strings with trailing zeroes.

--- app.py
+++ app.py
@@ -4,6 +4,29 @@ from board import Board
 from solver import solve
 import string
 
+def build_puzzle_string(request_args):
+    """Returns a tuple of (success, string).
+
+    Request_args is a dictionary of the URL request values.
+
+    The first element in the tuple is True iff the string was built without errors.
+    The second element is an error message if there was one,
+        and the puzzle string otherwise.
+    """
+    board_letters = []
+    digits = set(string.digits)
+    for row in xrange(9):
+        for col in xrange(9):
+            cell = request_args.get(str(row) + str(col), '')
+            if cell == "" or cell == "0":
+                board_letters.append("0")
+            elif cell not in digits:
+                error = cell + " is not a number in the range 0-9. "
+                return (False, error)
+            else:
+                board_letters.append(cell)
+    return (True, "".join(board_letters))
+
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -16,35 +39,30 @@ def index():
         return render_template('grid.html')
     digits = set(string.digits)
     BASE_ERROR = "Please try again or enter your board values manually."
-    # TODO: Handle strings > 81 chars
+    error = '' # Error message to output on page, if there is one
     if 'puzzle' in request.args:
         puzzle_string = request.args.get('puzzle')
-        if (set(puzzle_string) - digits) != set():
-            # puzzle_string contains non-digit characters
-            msg = "That string contains non-numeric characters. " + BASE_ERROR
-            return render_template('grid.html', error=msg)
-        if len(puzzle_string) < 81:
+        if len(puzzle_string) > 81:
+            error = "Puzzle strings must contain no more than 81 characters. " + BASE_ERROR
+        elif (set(puzzle_string) - digits) != set():
+            error = "That string contains non-numeric characters. " + BASE_ERROR
+        elif len(puzzle_string) < 81:
             # Pad short string with trailing zeroes
             puzzle_string += "0" * (81 - len(puzzle_string))
         board_array = Board.string_to_array(puzzle_string)
-    elif len(request.args) >= 81:
-        board_letters = []
-        letter_set = set(string.digits)
-        for row in xrange(9):
-            for col in xrange(9):
-                cell = request.args.get(str(row) + str(col), '')
-                if cell == "" or cell == "0":
-                    board_letters.append("0")
-                elif cell not in digits:
-                    msg = "'{}' is not a number in the range 0-9. ".format(cell) + BASE_ERROR
-                    return render_template('grid.html', error=msg)
-            else:
-                board_letters.append(cell)
-        board_array = Board.string_to_array("".join(board_letters))
-    b = Board(board_array)
-    try:
-        solved = solve(b)
-    except ValueError as e:
-        msg = str(e) + " " + BASE_ERROR
-        return render_template('grid.html', error=msg)
+    else:
+        # Any extra arguments are ignored
+        success, result_string = build_puzzle_string(request.args)
+        if success:
+            board_array = Board.string_to_array(result_string)
+        else:
+            error = result_string + BASE_ERROR
+    if not error:
+        b = Board(board_array)
+        try:
+            solved = solve(b)
+        except ValueError as e:
+            error = str(e) + " " + BASE_ERROR
+    if error: # Different from the above check because solve() might raise an error
+        return render_template('grid.html', error=error)
     return render_template('result.html', output=solved.to_dict())
